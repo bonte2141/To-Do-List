87cba893d404dbd23e31de994ef4c6216bb63aef
bonte <bonte.lucas234@gmail.com>
2024-03-24 23:08:52 +0200

    todolist

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>To-Do List</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  <h1>To-Do List</h1>
+  <div id="task-list"></div>
+  <form id="task-form">
+    <input type="text" id="task-input" placeholder="Task name" required>
+    <input type="date" id="date-input" required>
+    <button type="submit">Add Task</button>
+  </form>
+
+  <script src="script.js"></script>
+</body>
+</html>
